155608f8e21a10f67bc148ca5aca1540a13b2554
caomengxuan666 <2507560089@qq.com>
2024-10-27 22:23:58 +0800

    现在可以手动设置库的日志等级，不需要每一次log都进行重复添加log级别的操作了，设置一次之后可以在缺省级别参数的情况下进行打印。更新了新版本的日志库

--- example/main.cpp
+++ example/main.cpp
@@ -1,12 +1,32 @@
-﻿#include <LogManager.hpp>
-#include <LogLevel.hpp>
+﻿#include <LogLevel.hpp>
+#include <LogManager.hpp>
 
-int main() {
+void manualTest() {
+    cmx::logManager.setCurrentLevel(LogLevel::Info);
+    cmx::logManager.log("manualTest");
+
+    cmx::logManager.setFileAppender("logAuto.txt");
+    cmx::logManager.setConsoleAppender();
+    cmx::logManager.setCurrentLevel(LogLevel::Debug);
+    std::string message = "Debug message";
+    cmx::logManager.log(message);
+    cmx::logManager.setCurrentLevel(LogLevel::Error);
+    cmx::logManager.log("Error message");
+    cmx::logManager.setCurrentLevel(LogLevel::Fatal);
+    cmx::logManager.log("Fatal message");
+    cmx::logManager.setCurrentLevel(LogLevel::Info);
+    cmx::logManager.log("Info message");
+    cmx::logManager.setCurrentLevel(LogLevel::Warning);
+    cmx::logManager.log("Warning message");
+}
+
+//手动版本
+void autoTest() {
     // 设置文件日志记录器
     cmx::logManager.setFileAppender("log.txt");
     // 设置控制台日志记录器
     cmx::logManager.setConsoleAppender();
-    std::string message = "It's Cmx's LOG SYSTEM!";
+    std::string message = "AUTO TEST";
     cmx::logManager.log(message, cmx::Log::LogLevel::Info);
 
     // 更多测试消息
@@ -14,13 +34,18 @@ int main() {
             "This is a Debug message",
     };
 
-    for (const auto& msg : testMessages) {
+    for (const auto &msg: testMessages) {
         cmx::logManager.log(msg, cmx::LogLevel::Debug);
         cmx::logManager.log(msg, cmx::LogLevel::Info);
         cmx::logManager.log(msg, cmx::LogLevel::Warning);
         cmx::logManager.log(msg, cmx::LogLevel::Error);
         cmx::logManager.log(msg, cmx::LogLevel::Fatal);
     }
+}
 
-    return 0;
+int main() {
+    autoTest();
+    cmx::logManager.setCurrentLevel(LogLevel::Info);
+    cmx::logManager.log("End of Autotest\t\t\n ************************************************");
+    manualTest();
 }
--- include/LogManager.hpp
+++ include/LogManager.hpp
@@ -1,29 +1,32 @@
-#pragma once
+﻿#pragma once
 
+#include "LogLevel.hpp"
+#include "console_appender.hpp"
+#include "file_appender.hpp"
+#include "formatter.hpp"
 #include <iostream>
 #include <memory>
 #include <string>
-#include "file_appender.hpp"
-#include "console_appender.hpp"
-#include "formatter.hpp"
-#include "LogLevel.hpp"
 
 namespace cmx::Log {
 
     class LogManager {
+    private:
+        LogLevel currentLevel;
+
     public:
         // 获取单例实例
-        static LogManager& getInstance() {
-            static LogManager instance;  // 静态实例，保证全局唯一
+        static LogManager &getInstance() {
+            static LogManager instance;// 静态实例，保证全局唯一
             return instance;
         }
 
         // 禁止拷贝构造和赋值操作符
-        LogManager(const LogManager&) = delete;
-        LogManager& operator=(const LogManager&) = delete;
+        LogManager(const LogManager &) = delete;
+        LogManager &operator=(const LogManager &) = delete;
 
         // 设置文件日志记录器
-        void setFileAppender(const std::string& filePath) {
+        void setFileAppender(const std::string &filePath) {
             fileAppender = std::make_unique<FileAppender>(filePath);
             fileAppender->setFormatter(std::make_unique<SimpleFormatter>());
         }
@@ -34,8 +37,16 @@ namespace cmx::Log {
             consoleAppender->setFormatter(std::make_unique<SimpleFormatter>());
         }
 
+        void setCurrentLevel(LogLevel level) {
+            currentLevel = level;
+        }
+
+        void log(const std::string &message) {
+            log(message, currentLevel);
+        }
+
         // 日志输出
-        void log(const std::string& message, LogLevel level) {
+        void log(const std::string &message, LogLevel level) {
             if (fileAppender) {
                 std::string formattedMessage = formatMessage(message, level);
                 fileAppender->append(formattedMessage);
@@ -47,35 +58,42 @@ namespace cmx::Log {
             }
         }
 
+        void setBufferSize(size_t msg_buffer) {
+
+        }
+
     private:
         // 私有构造函数，确保外部无法直接实例化
         LogManager() = default;
 
         // 格式化消息
-        std::string formatMessage(const std::string& message, LogLevel level) {
-            const char* color = LogLevelImpl::toColor(level);
-            const char* resetColor = colors::Reset;
+        std::string formatMessage(const std::string &message, LogLevel level) {
+
+
+            const char *color = LogLevelImpl::toColor(level);
+            const char *resetColor = colors::Reset;
             std::string logLevelStr = LogLevelImpl::toString(level);
 
-            char buffer[256];
+            char buffer[1024];
             snprintf(buffer, sizeof(buffer), "%s[%s]%s %s%s%s",
                      color, logLevelStr.c_str(), resetColor, color, message.c_str(), resetColor);
 
             return std::string(buffer);
         }
-        
+
 
         std::unique_ptr<FileAppender> fileAppender;
         std::unique_ptr<ConsoleAppender> consoleAppender;
     };
 
-
-}
+}// namespace cmx::Log
 // namespace cmx::Log
 
 
-namespace cmx{
+namespace cmx {
     using namespace cmx::Log;
     // 在这里初始化全局唯一的 LogManager 实例
-    static cmx::Log::LogManager& logManager = cmx::Log::LogManager::getInstance();
-}
+    static cmx::Log::LogManager &logManager = cmx::Log::LogManager::getInstance();
+}// namespace cmx
+
+using namespace cmx;
